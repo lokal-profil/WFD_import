56ea094924c4c6bafba812e7dd3a8d356d3c3139
lokal-profil <lokal.profil@gmail.com>
2017-04-28 02:07:51 +0300

    import whole

--- tests/test_wfdbot.py
+++ tests/test_wfdbot.py
@@ -4,8 +4,7 @@ from __future__ import unicode_literals
 
 import unittest
 
-from WFD.WFDBase import UnmappedValueError
-from WFD.WFDBase.WfdBot import validate_mapping
+from WFD.WFDBase import WfdBot, UnmappedValueError
 
 
 class TestValidateMapping(unittest.TestCase):
@@ -23,29 +22,36 @@ class TestValidateMapping(unittest.TestCase):
             self.fail(e)
 
     def test_validate_mapping_both_empty(self):
-        try:
-            validate_mapping([], [], self.label)
-        except UnmappedValueError:
-            self.fail("validate_mapping() raised UnmappedValueError")
+        self.assert_not_raised(
+            WfdBot.validate_mapping({}, [], self.label),
+            UnmappedValueError)
 
     def test_validate_mapping_more_in_dict(self):
         found = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
         self.assert_not_raised(
-            validate_mapping(found, self.expected, self.label),
+            WfdBot.validate_mapping(found, self.expected, self.label),
             UnmappedValueError)
 
     def test_validate_mapping_less_in_dict(self):
         found = {'a': 1, 'b': 2, 'd': 4}
         with self.assertRaises(UnmappedValueError) as cm:
-            validate_mapping(found, self.expected, self.label)
+            WfdBot.validate_mapping(found, self.expected, self.label)
         self.assertEqual(
             str(cm.exception),
-            'The following values for "test" were not mapped: c')
+            'The following values for "test" were not mapped: [c]')
+
+    def test_validate_mapping_many_less_in_dict(self):
+        found = {'a': 1, 'd': 4}
+        with self.assertRaises(UnmappedValueError) as cm:
+            WfdBot.validate_mapping(found, self.expected, self.label)
+        self.assertEqual(
+            str(cm.exception),
+            'The following values for "test" were not mapped: [b, c]')
 
     def test_validate_mapping_same_in_dict(self):
         found = {'a': 1, 'b': 2, 'c': 3}
         self.assert_not_raised(
-            validate_mapping(found, self.expected, self.label),
+            WfdBot.validate_mapping(found, self.expected, self.label),
             UnmappedValueError)
 
     def test_validate_mapping_more_in_dicts(self):
@@ -54,7 +60,7 @@ class TestValidateMapping(unittest.TestCase):
             'second': {'a': 5, 'b': 6, 'c': 7, 'd': 8}
         }
         self.assert_not_raised(
-            validate_mapping(found, self.expected, self.label),
+            WfdBot.validate_mapping(found, self.expected, self.label),
             UnmappedValueError)
 
     def test_validate_mapping_less_in_all_dicts(self):
@@ -63,7 +69,7 @@ class TestValidateMapping(unittest.TestCase):
             'second': {'a': 5, 'b': 6, 'd': 8}
         }
         with self.assertRaises(UnmappedValueError):
-            validate_mapping(found, self.expected, self.label)
+            WfdBot.validate_mapping(found, self.expected, self.label)
         # don't check message since order is not guaranteed
 
     def test_validate_mapping_less_in_some_dicts(self):
@@ -72,10 +78,10 @@ class TestValidateMapping(unittest.TestCase):
             'second': {'a': 5, 'b': 6, 'd': 8}
         }
         with self.assertRaises(UnmappedValueError) as cm:
-            validate_mapping(found, self.expected, self.label)
+            WfdBot.validate_mapping(found, self.expected, self.label)
         self.assertEqual(
             str(cm.exception),
-            'The following values for "test" were not mapped: second, c')
+            'The following values for "test" were not mapped: (second, [c])')
 
     def test_validate_mapping_same_in_dicts(self):
         found = {
@@ -83,13 +89,13 @@ class TestValidateMapping(unittest.TestCase):
             'second': {'a': 5, 'b': 6, 'c': 7}
         }
         self.assert_not_raised(
-            validate_mapping(found, self.expected, self.label),
+            WfdBot.validate_mapping(found, self.expected, self.label),
             UnmappedValueError)
 
     def test_validate_mapping_label(self):
         found = {'a': 1, 'b': 2, 'd': 4}
         with self.assertRaises(UnmappedValueError) as cm:
-            validate_mapping(found, self.expected, 'test2')
+            WfdBot.validate_mapping(found, self.expected, 'test2')
         self.assertEqual(
             str(cm.exception),
-            'The following values for "test2" were not mapped: c')
+            'The following values for "test2" were not mapped: [c]')
